12d0f74af26748161e4e525f4094fcf3f42ca82e
patrickleomackey-is <plmackey@my.niacc.edu>
2020-10-20 19:51:52 -0500

    This is Unit 4 problem number 8!

--- unit4problem8/public_html/unit4problem8JS.js
+++ unit4problem8/public_html/unit4problem8JS.js
@@ -1,11 +1,18 @@
-/*Patrick Leo Mackey CIS 119 Prof. K. Miller unit4 problem8 */
+/*Patrick Leo Mackey CIS 119 Prof. K. Miller unit4 problem8 
+ * 
+ * I understand once I had this array sorted I could've just used numbers[x]
+ * 
+ * to indicate the least and maximum numbers in the array but I honestly need work
+ * 
+ * on writing the loops*/
+
 let numbers = [];
 for (let i = 0; i <= 19; i++ ){
     numbers.push(Number(prompt("Enter an integer")));
     
 }
 numbers.sort((a, b) => a - b);
-document.write(numbers.join("<br>"));
+//document.write(numbers.join("<br>"));
 
 let least = numbers[0];
 
@@ -26,3 +33,12 @@ for (i = 0; i < numbers.length; i++){
 }
 document.write("<br>"+ most + " is the most");
 
+let total = 0;
+
+for (i in numbers){ total += numbers[i];}
+
+document.write('<br>' + total + " is the sum of your integers");
+
+let aberage = (total / numbers.length);
+
+document.write("<br>"+ aberage + " is the average");
